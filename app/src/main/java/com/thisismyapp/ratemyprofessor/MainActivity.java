e84f5233e2221feafafb8088b6621b0b8706bd63
Jared Boswell <jaredbos@buffalo.edu>
2018-09-26 23:11:56 -0400

    Added a list of filler professor names to the strings.xml file. Added the list of names to the home page (MainActivity.java).

--- app/src/main/java/com/thisismyapp/ratemyprofessor/MainActivity.java
+++ app/src/main/java/com/thisismyapp/ratemyprofessor/MainActivity.java
@@ -2,12 +2,27 @@ package com.thisismyapp.ratemyprofessor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
+    ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView lv = (ListView)findViewById(R.id.listViewProfessors);
+        ArrayList<String> arrayProfessor = new ArrayList<>();
+        arrayProfessor.addAll(Arrays.asList(getResources().getStringArray(R.array.array_professor)));
+
+        adapter = new ArrayAdapter<>(
+                MainActivity.this,
+                android.R.layout.simple_list_item_1,
+                arrayProfessor);
+        lv.setAdapter(adapter);
     }
 }
