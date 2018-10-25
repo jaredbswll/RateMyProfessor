a3029521f900429fb0043ef7ae7bd1220083638f
dom <dtdistef@buffalo.edu>
2018-10-25 14:58:07 -0400

    -Keep google-services.json and new SubmitListener.java

---
+++ app/src/main/java/com/thisismyapp/ratemyprofessor/SubmitListener.java
@@ -0,0 +1,33 @@
+package com.thisismyapp.ratemyprofessor;
+
+import android.view.View;
+import android.widget.TextView;
+
+public class SubmitListener implements View.OnClickListener {
+
+    private ProfessorPage profPage;
+    private Professor prof;
+
+    public SubmitListener(ProfessorPage pp, Professor p){
+        profPage = pp;
+        prof = p;
+    }
+
+    @Override
+    public void onClick(View v) {
+        TextView userBox = (TextView) profPage.findViewById(R.id.user_name_input);
+        TextView ratingBox = (TextView) profPage.findViewById(R.id.user_rating_input);
+        TextView commentBox = (TextView) profPage.findViewById(R.id.commentBar);
+
+        String user = userBox.getText().toString();
+        String rating = ratingBox.getText().toString();
+        String comment = commentBox.getText().toString();
+
+        userBox.setText("");
+        ratingBox.setText("");
+        commentBox.setText("");
+
+        prof.addComment(user, rating, "Date", comment);
+
+    }
+}
