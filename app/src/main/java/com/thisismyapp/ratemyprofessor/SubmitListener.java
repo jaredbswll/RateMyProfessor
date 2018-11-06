8bdf9fb3155978aaf225aa9462d570ddef864854
omeroparis <omeropar@buffalo.edu>
2018-11-06 15:23:24 -0500

    - Quality of Life Changes: Added labels for users comments to better identify name, rating, etc. Made first two text fields on professor page only allow 1 line instead of infinite lines
    - Comment Functionality Changes:
    The rating that users can leave now must be inputted as a number
    Added code to the submit listener that will check if the users rating is 1 - 10 and if not a dialog pops up prompting the user, this uses a function I made in the professor page

--- app/src/main/java/com/thisismyapp/ratemyprofessor/SubmitListener.java
+++ app/src/main/java/com/thisismyapp/ratemyprofessor/SubmitListener.java
@@ -1,7 +1,10 @@
 package com.thisismyapp.ratemyprofessor;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SubmitListener implements View.OnClickListener {
 
@@ -23,12 +26,28 @@ public class SubmitListener implements View.OnClickListener {
         String rating = ratingBox.getText().toString();
         String comment = commentBox.getText().toString();
 
-        userBox.setText("");
-        ratingBox.setText("");
-        commentBox.setText("");
+        //Alert Dialog creates the popup to tell user to double check their input
+        AlertDialog.Builder prompt = new AlertDialog.Builder(profPage);
+        prompt.setCancelable(true);
+        prompt.setTitle("Check Rating");
+        prompt.setMessage("Please double check that you add a number between 1 and 10 for the professors rating");
+        prompt.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
 
-        prof.addComment(user, rating, prof.getProfClass(), comment);
-        profPage.addComment();
-
+        //Actually checks if the user enters a number that is 1 - 10 using function from professor page
+        int numRating = Integer.parseInt(rating);
+        if (profPage.checkUserRating(numRating) == true) {
+            userBox.setText("");
+            ratingBox.setText("");
+            commentBox.setText("");
+            prof.addComment(user, rating, prof.getProfClass(), comment);
+            profPage.addComment();
+        } else {
+            prompt.show();
+        }
     }
 }
